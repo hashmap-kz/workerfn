f79cdf0522d35cf69fe2795e74958a6340ab9ca4
alexey.zh <mailbox.stdio@gmail.com>
2025-02-23 09:08:11 +0500

    update-tests

--- pkg/versions/concurv1.go
+++ pkg/versions/concurv1.go
@@ -11,12 +11,15 @@ func ProcessConcurrentlyWithResultV1[T any, R any](
 	ctx context.Context,
 	tasks []T,
 	taskFunc func(context.Context, T) (R, error),
-	filterFunc func(R) bool, // Function to determine if a result should be included
 ) ([]R, []error) {
-	var wg sync.WaitGroup
-	results := make([]R, len(tasks))    // Preallocated slice for results
-	errors := make([]error, len(tasks)) // Preallocated slice for errors
+	type outcome struct {
+		result     R
+		hasContent bool
+		err        error
+	}
+	outcomes := make([]outcome, len(tasks)) // Preallocated slice for results and errors
 
+	var wg sync.WaitGroup
 	for i, task := range tasks {
 		wg.Add(1)
 		go func(index int, task T) {
@@ -27,38 +30,31 @@ func ProcessConcurrentlyWithResultV1[T any, R any](
 				return
 			}
 
-			select {
-			case <-ctx.Done(): // Check if context is canceled before running the task
+			result, err := taskFunc(ctx, task)
+
+			// Check again if context is canceled (during execution of taskFunc), before modifying results
+			if ctx.Err() != nil {
 				return
-			default:
-				result, err := taskFunc(ctx, task)
-				if err != nil {
-					errors[index] = err
-				} else {
-					results[index] = result
-				}
 			}
+
+			outcomes[index] = outcome{result: result, hasContent: true, err: err}
 		}(i, task)
 	}
 
 	wg.Wait() // Wait for all tasks to complete
 
-	// Apply the filter function to results
-	var filteredResults []R
-	for _, result := range results {
-		if filterFunc(result) { // Include only results that pass the filter
-			filteredResults = append(filteredResults, result)
-		}
-	}
-
-	// Filter nil errors for cleaner return
+	// Collect results and errors
 	var filteredErrors []error
-	for _, err := range errors {
-		if err != nil {
-			filteredErrors = append(filteredErrors, err)
+	var filteredResults []R
+	for _, o := range outcomes {
+		if o.err != nil {
+			filteredErrors = append(filteredErrors, o.err)
+		} else {
+			if o.hasContent {
+				filteredResults = append(filteredResults, o.result)
+			}
 		}
 	}
-
 	return filteredResults, filteredErrors
 }
 
@@ -67,10 +63,13 @@ func ProcessConcurrentlyWithResultAndLimitV1[T any, R any](
 	workerLimit int,
 	tasks []T,
 	taskFunc func(context.Context, T) (R, error),
-	filterFunc func(R) bool, // Function to determine if a result should be included
 ) ([]R, []error) {
-	results := make([]R, len(tasks))    // Preallocated slice for results
-	errors := make([]error, len(tasks)) // Preallocated slice for errors
+	type outcome struct {
+		result     R
+		hasContent bool
+		err        error
+	}
+	outcomes := make([]outcome, len(tasks)) // Preallocated slice for results and errors
 
 	taskChan := make(chan int, len(tasks)) // Channel to distribute tasks
 	var wg sync.WaitGroup
@@ -94,11 +93,13 @@ func ProcessConcurrentlyWithResultAndLimitV1[T any, R any](
 
 					// Execute task and store result or error
 					result, err := taskFunc(ctx, tasks[index])
-					if err != nil {
-						errors[index] = err
-					} else {
-						results[index] = result
+
+					// Check again if context is canceled (during execution of taskFunc), before modifying results
+					if ctx.Err() != nil {
+						return
 					}
+
+					outcomes[index] = outcome{result: result, hasContent: true, err: err}
 				}
 			}
 		}()
@@ -116,21 +117,17 @@ func ProcessConcurrentlyWithResultAndLimitV1[T any, R any](
 
 	wg.Wait() // Wait for all workers to finish
 
-	// Apply the filter function to results
-	var filteredResults []R
-	for _, result := range results {
-		if filterFunc(result) { // Include only results that pass the filter
-			filteredResults = append(filteredResults, result)
-		}
-	}
-
-	// Filter nil errors for cleaner return
+	// Collect results and errors
 	var filteredErrors []error
-	for _, err := range errors {
-		if err != nil {
-			filteredErrors = append(filteredErrors, err)
+	var filteredResults []R
+	for _, o := range outcomes {
+		if o.err != nil {
+			filteredErrors = append(filteredErrors, o.err)
+		} else {
+			if o.hasContent {
+				filteredResults = append(filteredResults, o.result)
+			}
 		}
 	}
-
 	return filteredResults, filteredErrors
 }
--- pkg/versions/concurv1_test.go
+++ pkg/versions/concurv1_test.go
@@ -14,11 +14,6 @@ func mockTask1(ctx context.Context, input int) (int, error) {
 	return 0, errors.New("odd number error") // Return error for odd numbers
 }
 
-// Filter function for testing
-func mockFilter1(result int) bool {
-	return result > 4 // Only include results greater than 4
-}
-
 func BenchmarkProcessConcurrentlyWithResultV1(b *testing.B) {
 	tasks := make([]int, tasks)
 	for i := 0; i < len(tasks); i++ {
@@ -28,7 +23,7 @@ func BenchmarkProcessConcurrentlyWithResultV1(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		ProcessConcurrentlyWithResultV1(ctx, tasks, mockTask1, mockFilter1)
+		ProcessConcurrentlyWithResultV1(ctx, tasks, mockTask1)
 	}
 }
 
@@ -41,6 +36,6 @@ func BenchmarkProcessConcurrentlyWithResultAndLimitV1(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		ProcessConcurrentlyWithResultAndLimitV1(ctx, workers, tasks, mockTask1, mockFilter1)
+		ProcessConcurrentlyWithResultAndLimitV1(ctx, workers, tasks, mockTask1)
 	}
 }
